8b2331991440d92d33bacb7124a90567892d3a2e
youzhen <yl301114@gmail.com>
2025-05-20 15:59:55 +0100

    表格調整

--- public/js/common.js
+++ public/js/common.js
@@ -5,8 +5,15 @@ function table() {
         let th_height = $(this).find(".table__main").find("th").children("span").height() + 40;
         $(this).find(".table__function").find("th").children("span").css("height",th_height);
         $(this).find(".table__main").find(".table__tr").each(function(i) {
-            let td_height = $(this).find("td").height();
-            $(this).parents(".table__inner").find(".table__function").find(".c-table-btns__wrapper").eq(i).css("height",td_height);
+            let length = $(this).find("td").length;
+            let max_height = 0;
+            for(let j=0;j<length;j++) {
+                let td_height = $(this).find("td").eq(j).find("span").height() + 60;
+                if(td_height >= max_height) {
+                    max_height = td_height;
+                }
+            }
+            $(this).parents(".table__inner").find(".table__function").find(".c-table-btns__wrapper").eq(i).css("height",max_height);
         })
 
         let func_width = $(this).find(".table__function").width();
