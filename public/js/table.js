11835c1e9f29829dd4f2d8944d19dad62ab41872
youzhen <yl301114@gmail.com>
2025-06-20 18:34:12 +0100

    table間距

--- public/js/table.js
+++ public/js/table.js
@@ -2,21 +2,17 @@
 table();
 function table() {
     $(".table__inner.has-func").each(function() {
-        let th_height = $(this).find(".table__main").find("th").children("span").height() + 40;
+        let th_height = $(this).find(".table__main").find("th").children("span").height() + 20;
         $(this).find(".table__function").find("th").children("span").css("height",th_height);
         $(this).find(".table__main").find(".table__tr").each(function(i) {
             let length = $(this).find("td").length;
-            console.log("i=" + i + ",length=" + length);
             let max_height = 0;
             for(let j=0;j<length;j++) {
-                let td_height = $(this).find("td").eq(j).find("span").height() + 60;
-                console.log("j=" + j + ",td_height=" + td_height + ",max_height=" + max_height);
+                let td_height = $(this).find("td").eq(j).find("span").height() + 20;
                 if(td_height >= max_height) {
                     max_height = td_height;
                 }
             }
-            console.log("final max_height=" + max_height);
-            console.log("end of this tr");
             $(this).parents(".table__inner").find(".table__function").find(".c-table-btns__wrapper").eq(i).css("height",max_height);
         })
 
